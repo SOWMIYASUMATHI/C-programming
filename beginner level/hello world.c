16867fa78898d424ea546f3320bf2db05f98f0bc
SOWMIYASUMATHI <37831179+SOWMIYASUMATHI@users.noreply.github.com>
2018-03-27 14:50:09 +0530

    Create hello world.c

---
+++ beginner level/hello world.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+		int a;
+	printf("\n enter the number:");
+	scanf("%d",&a);
+	if(a==0)
+	{
+		printf("zero");
+	}else if(a>0)
+	{
+		printf("positive");
+	}else
+	{
+		printf("negative");
+	}
+	// your code goes here
+	return 0;
+	// your code goes here
+}
